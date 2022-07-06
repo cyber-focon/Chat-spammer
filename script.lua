3465dbd42ef9d1c28ad0453b8342a74e337ee786
cyber-focon <84711019+cyber-focon@users.noreply.github.com>
2022-07-06 19:13:35 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,12 +1,18 @@
--- Gui Générer grave a Gui to Lua
--- Le reste est fait par moi meme
-
 local ScreenGui = Instance.new("ScreenGui")
 local backgroud = Instance.new("Frame")
 local message = Instance.new("TextBox")
 local title = Instance.new("TextLabel")
 local toggle = Instance.new("TextButton")
 local quit = Instance.new("TextButton")
+local slider = Instance.new("Frame")
+local sliderSelect = Instance.new("Frame")
+local sliderButtun = Instance.new("TextButton")
+local sliderInfo = Instance.new("TextLabel")
+local runService = game:GetService("RunService")
+local uis = game:GetService("UserInputService")
+local spam = false
+local lock = true
+local speed = 5
 
 ScreenGui.Parent = game.CoreGui
 
@@ -21,10 +27,11 @@ backgroud.Draggable = true
 message.Name = "message"
 message.Parent = backgroud
 message.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-message.Position = UDim2.new(0.0954092219, 0, 0.195761234, 0)
-message.Size = UDim2.new(0, 427, 0, 27)
+message.BorderSizePixel = 2
+message.Position = UDim2.new(0.0859395266, 0, 0.195761263, 0)
+message.Size = UDim2.new(0, 437, 0, 27)
 message.Font = Enum.Font.SourceSans
-message.Text = "Message"
+message.Text = "Write your message!"
 message.TextColor3 = Color3.fromRGB(255, 255, 255)
 message.TextSize = 19.000
 
@@ -60,6 +67,35 @@ quit.TextColor3 = Color3.fromRGB(255, 255, 255)
 quit.TextSize = 30.000
 quit.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
 
+slider.Name = "slider"
+slider.Parent = backgroud
+slider.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+slider.BackgroundTransparency = 0.010
+slider.BorderSizePixel = 2
+slider.Position = UDim2.new(0.0859395266, 0, 0.451492548, 0)
+slider.Size = UDim2.new(0, 437, 0, 25)
+
+sliderButtun.Name = "sliderButtun"
+sliderButtun.Parent = slider
+sliderButtun.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+sliderButtun.Position = UDim2.new(0.473067939, 0, -0.400000006, 0)
+sliderButtun.Size = UDim2.new(0, 20, 0, 46)
+sliderButtun.Font = Enum.Font.SourceSans
+sliderButtun.Text = ""
+sliderButtun.TextColor3 = Color3.fromRGB(0, 0, 0)
+sliderButtun.TextSize = 14.000
+
+sliderInfo.Name = "sliderInfo"
+sliderInfo.Parent = slider
+sliderInfo.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+sliderInfo.BorderSizePixel = 0
+sliderInfo.Position = UDim2.new(0, 0, 1.44000006, 0)
+sliderInfo.Size = UDim2.new(0, 426, 0, 17)
+sliderInfo.Font = Enum.Font.SourceSans
+sliderInfo.Text = "Time sleep: 5s"
+sliderInfo.TextColor3 = Color3.fromRGB(255, 255, 255)
+sliderInfo.TextSize = 14.000
+
 toggle.MouseButton1Down:connect(function()
 	if spam then
 		spam = false
@@ -83,7 +119,31 @@ quit.MouseButton1Down:connect(function()
 	end
 end)
 
-while true do wait(0.5)
+sliderButtun.MouseButton1Down:Connect(function()
+	lock = false
+end)
+
+uis.InputEnded:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 and backgroud.Active and backgroud.Visible then
+		lock = true
+	end
+end)
+
+uis.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement then
+		if lock == false and backgroud.Active and backgroud.Visible then
+			local mouseLoc = uis:GetMouseLocation()
+			local btnPos = sliderButtun.Position
+			local relativePos = mouseLoc.X-slider.AbsolutePosition.X
+			local percentage = math.clamp(relativePos/slider.AbsoluteSize.X, 0, 1)
+			sliderButtun.Position = UDim2.new(percentage - 0.0228833, 0, btnPos.Y.Scale, btnPos.X.Offset)
+			sliderInfo.Text = "Time sleep: " .. math.round(percentage*10) .. "s"
+			speed = percentage*10
+		end
+	end
+end)
+
+while wait(speed) do
 	if backgroud.Active and backgroud.Visible and spam then
 		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message.Text, "all")
 	end
